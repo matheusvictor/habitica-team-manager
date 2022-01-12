ff4ac74e68473a155aa6a8db687f3a07c0b750aa
Matheus Victor <matheusvictor.salles@gmail.com>
2022-01-12 12:00:40 -0300

    refactor: moved Retrofit implementation to remote package

--- app/src/main/java/com/matheusvictor/habiticateammanager/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/matheusvictor/habiticateammanager/service/repository/remote/RetrofitClient.kt
@@ -1,46 +1,46 @@
-package com.matheusvictor.habiticateammanager.service.repository
+package com.matheusvictor.habiticateammanager.service.repository.remote
 
 import com.matheusvictor.habiticateammanager.service.constants.AppConstants
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitClient {
+class RetrofitClient private constructor() {
 
     companion object {
 
-        private lateinit var retrofit: Retrofit
-        private const val BASE_URL = AppConstants.API.BASE_URL
+        private lateinit var mRetrofit: Retrofit
+
         private var username: String = ""
         private var apiToken: String = ""
+        private const val BASE_URL = AppConstants.API.BASE_URL
 
         private fun getRetrofitInstance(): Retrofit {
 
             val httpClient = OkHttpClient.Builder()
             httpClient.addInterceptor { chain ->
-                val request =
-                    chain.request()
-                        .newBuilder()
-                        .addHeader(AppConstants.HEADER.USERNAME, this.username)
-                        .addHeader(AppConstants.HEADER.API_TOKEN, this.apiToken)
-                        .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .addHeader(AppConstants.HEADER.USERNAME, username)
+                    .addHeader(AppConstants.HEADER.API_TOKEN, apiToken)
+                    .build()
                 chain.proceed(request)
             }
 
-            if (!::retrofit.isInitialized) {
-                retrofit = Retrofit.Builder()
+            if (!Companion::mRetrofit.isInitialized) {
+                mRetrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .client(httpClient.build())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
             }
 
-            return retrofit
+            return mRetrofit
         }
 
         fun addHeaders(username: String, apiToken: String) {
-            this.username = username
-            this.apiToken = apiToken
+            Companion.username = username
+            Companion.apiToken = apiToken
         }
 
         fun <S> createService(serviceClass: Class<S>): S {
